d617397778be546fac236589f08071219a7271dd
kverbosky <40339091+kverbosky@users.noreply.github.com>
2025-09-30 13:44:00 -0400

    Add files via upload

---
+++ Lab2.3Verbosky0.1.py
@@ -0,0 +1,33 @@
+#print("Kuon Verbosky", "Period 8", "9/23/25", sep = "\n") 
+
+# Task 1
+print("Task #1")
+fullName = input("What is your full name?\t")
+firstName = fullName[:fullName.find(" ", 0)]
+lastName = fullName[fullName.find(" ", 0)+1:]
+
+print(firstName.title(), lastName.title()) #proper title print
+print(firstName[0:1].lower() + firstName[1:].upper() +\ 
+", " + lastName[0:1].lower() + lastName[1:].upper()) #flipped title cases print
+print()
+
+print("Task #2")
+phrase = "Once you start down the dark path, forever will it dominate your destiny."
+phraseUppered = phrase.upper()
+#phraseEncrypted = phraseUppered.replace()
+
+#print("Task #3")
+
+
+
+#print("Task #4")
+
+
+
+#print("Task #5")
+
+
+
+#print("Task #6")
+
+
